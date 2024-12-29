3697db0ddefd0d7600b0f214b13e07b0a12b1c09
Okan Akdoğan <okanakdogan@Okans-MacBook-Air.local>
2024-12-29 14:51:53 +0300

    Switch to PostgreSQL

--- config.py
+++ config.py
@@ -4,14 +4,14 @@ from datetime import timedelta
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev'
     
-    # PostgreSQL URL'sini düzenle
+    # PostgreSQL bağlantısı
     DATABASE_URL = os.environ.get('DATABASE_URL')
     if DATABASE_URL:
         if DATABASE_URL.startswith("postgres://"):
             DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
         SQLALCHEMY_DATABASE_URI = DATABASE_URL
     else:
-        SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+        SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:postgres@localhost:5432/forum'
     
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     PERMANENT_SESSION_LIFETIME = timedelta(days=7) 
