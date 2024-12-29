7a232370784ee094499dd8044a5ef4e1f6fa0e6e
Okan Akdoğan <okanakdogan@Okans-MacBook-Air.local>
2024-12-29 15:21:56 +0300

    Add SSL mode requirement to database URI

--- config.py
+++ config.py
@@ -5,9 +5,12 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev'
     
     # PostgreSQL bağlantısı
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'postgresql://db_450mt_forum_user:ys3L3IHbALZHlSypA16tKns9nysdk6XK@dpg-ctohogtsvqrc73b8htkg-a.frankfurt-postgres.render.com/db_450mt_forum')
-    if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
-        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
+    base_uri = os.environ.get('DATABASE_URL', 'postgresql://db_450mt_forum_user:ys3L3IHbALZHlSypA16tKns9nysdk6XK@dpg-ctohogtsvqrc73b8htkg-a.frankfurt-postgres.render.com/db_450mt_forum')
+    if base_uri.startswith("postgres://"):
+        base_uri = base_uri.replace("postgres://", "postgresql://", 1)
+    
+    # SSL parametrelerini ekle
+    SQLALCHEMY_DATABASE_URI = f"{base_uri}?sslmode=require"
     
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     PERMANENT_SESSION_LIFETIME = timedelta(days=7) 
