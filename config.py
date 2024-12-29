9f7c86f6392b58cc00d19b97f6fbd9484a379b49
Okan Akdoğan <okanakdogan@Okans-MacBook-Air.local>
2024-12-29 15:18:49 +0300

    Fix database URI configuration

--- config.py
+++ config.py
@@ -5,14 +5,9 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev'
     
     # PostgreSQL bağlantısı
-    DATABASE_URL = os.environ.get('DATABASE_URL')
-    if DATABASE_URL:
-        if DATABASE_URL.startswith("postgres://"):
-            DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
-        SQLALCHEMY_DATABASE_URI = DATABASE_URL
-    else:
-        # Render'da PostgreSQL bağlantısı
-        SQLALCHEMY_DATABASE_URI = os.environ.get('RENDER_DATABASE_URL', 'sqlite:///site.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'postgresql://db_450mt_forum_user:ys3L3IHbALZHlSypA16tKns9nysdk6XK@dpg-ctohogtsvqrc73b8htkg-a.frankfurt-postgres.render.com/db_450mt_forum')
+    if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
     
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     PERMANENT_SESSION_LIFETIME = timedelta(days=7) 
