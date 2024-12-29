74afb5953466f8d94ec3f90d5af48756e8f6dcef
Okan Akdoğan <okanakdogan@Okans-MacBook-Air.local>
2024-12-30 00:22:09 +0300

    Blueprint yapısı düzenlendi ve logging eklendi

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,9 @@ from flask_bcrypt import Bcrypt
 from flask_mail import Mail
 from flask_migrate import Migrate
 from config import Config
+import logging
+from logging.handlers import RotatingFileHandler
+import os
 
 db = SQLAlchemy()
 bcrypt = Bcrypt()
@@ -33,6 +36,18 @@ def create_app(config_class=Config):
     app.register_blueprint(main)
     app.register_blueprint(auth)
     app.register_blueprint(errors)
+
+    if not app.debug:
+        if not os.path.exists('logs'):
+            os.mkdir('logs')
+        file_handler = RotatingFileHandler('logs/450mtforum.log', maxBytes=10240, backupCount=10)
+        file_handler.setFormatter(logging.Formatter(
+            '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'
+        ))
+        file_handler.setLevel(logging.INFO)
+        app.logger.addHandler(file_handler)
+        app.logger.setLevel(logging.INFO)
+        app.logger.info('450MT Forum başlatılıyor')
 
     with app.app_context():
         db.create_all()
